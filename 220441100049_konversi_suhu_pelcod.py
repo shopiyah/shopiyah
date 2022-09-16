02952d1229787f52e2f25c7a1cfb7c5bedb5ab4e
shopiyah <113223747+shopiyah@users.noreply.github.com>
2022-09-16 10:05:13 +0700

    Add files via upload

---
+++ 220441100049_konversi_suhu_pelcod.py
@@ -0,0 +1,10 @@
+celcius = float(input("Masukan suhu Celcius? : "))
+
+fahrenheit = celcius * 9 / 5 + 32
+kelvin = celcius + 273.15
+reamur = 4/5 * celcius
+
+print(celcius,"Celcius","=",(fahrenheit), "Fahrenheit")
+print(celcius,"Celcius","=",(kelvin), "Kelvin")
+print(celcius,"Celcius","=",(reamur), "Reamur")
+
